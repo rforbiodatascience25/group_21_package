e998b932963bad1dd8503e5b33150fb564d0801a
sairenukac <s252633@dtu.dk>
2025-10-28 09:29:08 +0100

    Codons data is included here

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Codons for Amino Acids
+#'
+#' This data describes which codons code for different amino acids
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codons"
